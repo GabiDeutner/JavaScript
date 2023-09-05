49e3e1576401624bd5ec59287d20ac7c382e280c
Gabriela Paulini Deutner <deutnerg@gmail.com>
2023-09-05 13:21:42 -0300

    Create funcoes.js

---
+++ Estudos_Origamid/Funcoes/funcoes.js
@@ -0,0 +1,12 @@
+//FUNÇÕES
+//Bloco de código que pode ser executado e reutilizado. Valores podem ser passados por uma função e a mesma retorna outro valor.
+
+function areaQuadrado(lado) {
+  return lado * lado;
+}
+
+areaQuadrado(4) // 16
+areaQuadrado(5) // 25
+areaQuadrado(2) // 4
+
+//Chamada de function declaration
